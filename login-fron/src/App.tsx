dad167f25d5b37d987eac122ea91da2aed5569ff
emersson_11 <emerssonramirez510@gmail.com>
2025-03-26 13:39:50 -0500

    configuraciones del proyecto

--- login-fron/src/App.tsx
+++ login-fron/src/App.tsx
@@ -3,27 +3,31 @@ import { routes, TipoLayout } from "./router/router";
 import { ProviderContext } from "./context/providers/ProviderContext";
 import { LoginLayout } from "./components/layouts/LoginLayout";
 import { HomeLayout } from "./components/layouts/HomeLayout";
+import {HeroUIProvider} from "@heroui/react";
+import {QueryClientProvider} from '@tanstack/react-query'
+import {queryClient} from "./config/queryClient.ts";
 
 export const App = () => {
-  return (
-    <>
-      <ProviderContext>
-        <Routes>
-          {routes.map(({ element, path, useLayout }) => (
-            <Route
-              key={path}
-              path={path}
-              element={
-                useLayout === TipoLayout.LOGIN_LAYOUT ? (
-                  <LoginLayout> {element} </LoginLayout>
-                ) : (
-                  <HomeLayout>{element}</HomeLayout>
-                )
-              }
-            />
-          ))}
-        </Routes>
-      </ProviderContext>
-    </>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <HeroUIProvider>
+                <ProviderContext>
+                    <Routes>
+                        {routes.map(({ element, path, useLayout }) => (
+                            <Route
+                                key={path}
+                                path={path}
+                                element={
+                                    useLayout === TipoLayout.LOGIN_LAYOUT ? (
+                                        <LoginLayout> {element} </LoginLayout>
+                                    ) : (
+                                        <HomeLayout>{element}</HomeLayout>
+                                    )}
+                            />
+                        ))}
+                    </Routes>
+                </ProviderContext>
+            </HeroUIProvider>
+        </QueryClientProvider>
+    );
 };
